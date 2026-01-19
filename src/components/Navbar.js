5c1d15188925bb89d15edb5fb02d2c2ed08aa7c6
24021479-ChuaXinfang <24021479@myrp.edu.sg>
2026-01-19 15:30:44 +0800

    Created Navbar with styling in Navbar.js and App.css respectively

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,37 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  /* TODO: Complete the navbar 
-    - add links to CardList and AddCard pages 
-    - style as a navbar UI */
-
   return (
-    <header>
-      <strong>Card App</strong>
-      <nav>
-        <NavLink to="/" end>
-          Home
+    <header className="navbar">
+      {/* Left side */}
+      <div className="navbar-left">
+        <NavLink to="/" className="logo">
+          Card Management App
         </NavLink>
-      </nav>
+
+        <NavLink
+          to="/cards"
+          end
+          className={({ isActive }) =>
+              isActive ? "nav-link button cardlist-btn active" : "nav-link button cardlist-btn"
+          }
+        >
+          My CardList
+        </NavLink>
+      </div>
+
+      {/* Right side */}
+      <div className="navbar-right">
+        <NavLink
+          to="/cards/new"
+          className={({ isActive }) =>
+            isActive ? "nav-link button add-btn active" : "nav-link button add-btn"
+          }
+        >
+          + Add Card
+        </NavLink>
+      </div>
     </header>
   );
 }
+
